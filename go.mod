6a74e74ca0c6fdabab7d8faaef2914953b4cac41
Daniel Fucci <daniel.m.fucci@gmail.com>
2023-09-29 22:27:48 -0300

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/danikyl/prometheus
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	w.Write([]byte("Hello, " + r.URL.Query().Get("name")))
+}
+
+func main() {
+	http.HandleFunc("/hello", basicHandler)
+	log.Println("Listening on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
